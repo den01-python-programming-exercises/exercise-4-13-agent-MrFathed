8bfe9fa045fa5424a6ba4d5268aff243d8476b08
Justin Turner <jmturner638@gmail.com>
2021-08-04 14:09:59 -0500

    First Try

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,14 @@
+from agent import Agent
+
 def main():
     #write your code below this line
+    bond = Agent("James", "Bond")
+
+    print(bond)
+
+    ionic = Agent("Ionic", "Bond")
+    print(ionic)
+
 
 if __name__ == '__main__':
     main()
